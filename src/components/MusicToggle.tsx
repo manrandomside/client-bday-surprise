5a0d9312aceb0b2fb022f19c4c1f28603e97242e
manrandomside <cussrombymangans1@gmail.com>
2026-04-15 09:00:09 +0700

    feat: add bg-music.mp3 in folder public\music

--- src/components/MusicToggle.tsx
+++ src/components/MusicToggle.tsx
@@ -42,7 +42,7 @@ export default function MusicToggle() {
   }, [isPlaying]);
 
   return (
-    <div className="fixed bottom-4 right-4 z-50 flex flex-col items-end gap-2">
+    <div className="fixed bottom-4 right-4 z-[110] flex flex-col items-end gap-2">
       <AnimatePresence>
         {showHint && (
           <motion.div
